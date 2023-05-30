15e265b76b7829f22a726e371724e4840a8d6358
Lucas <107668539+lfomar1@users.noreply.github.com>
2023-05-30 03:02:46 -0400

    This is almost done, just a the slider left

--- script.js
+++ script.js
@@ -13,20 +13,101 @@ const colorPicker = document.querySelector("#color-picker");
 const colorMode = document.querySelector(".color-mode");
 const rainbowMode = document.querySelector(".rainbow-mode");
 const clear = document.querySelector(".clear");
-const sizeValue = document.querySelector(".sizeValue");
+const sizeValue = document.querySelector(".size-value");
 const sizeSlider = document.querySelector("#slider");
-const container = document.querySelector(".container");
-const grid = document.querySelector(".grid-item");
+const container = document.querySelector("#container");
 
 let mouseDown = false;
 
 // This changes the primary color
-function colorPick(e) {
-  const selectedColor = e.target.value;
-  defaultColor = selectedColor;
-}
 colorPicker.addEventListener("input", colorPick);
+function colorPick(e) {
+  let grid = Array.from(document.getElementsByClassName("grid-item"));
+  grid.forEach((grid) => {
+    function colorPicker() {
+      const selectedColor = e.target.value;
+      defaultColor = selectedColor;
+    }
+    grid.addEventListener("mousedown", function () {
+      mouseDown = true;
+      colorPicker();
+    });
+    grid.addEventListener("mouseover", function () {
+      if (mouseDown) {
+        colorPicker();
+      }
+    });
+    grid.addEventListener("mouseup", function () {
+      mouseDown = false;
+    });
+  });
+}
 
+//This code below makes it default color
+colorMode.addEventListener("click", defaultMode);
+function defaultMode() {
+  let grid = Array.from(document.getElementsByClassName("grid-item"));
+  grid.forEach((grid) => {
+    function normalMode() {
+      grid.style.backgroundColor = defaultColor;
+      defaultColor = PRIMARYDARK;
+    }
+    grid.addEventListener("mousedown", function () {
+      mouseDown = true;
+      normalMode();
+    });
+    grid.addEventListener("mouseover", function () {
+      if (mouseDown) {
+        normalMode();
+      }
+    });
+    grid.addEventListener("mouseup", function () {
+      mouseDown = false;
+    });
+  });
+}
+
+// This changes to rainbow mode
+rainbowMode.addEventListener("click", rainbowModi);
+function rainbowModi() {
+  let grid = Array.from(document.getElementsByClassName("grid-item"));
+  grid.forEach((grid) => {
+    function rainbowModel() {
+      const randomColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+      grid.style.backgroundColor = randomColor;
+      return randomColor;
+    }
+    grid.addEventListener("mousedown", function () {
+      mouseDown = true;
+      rainbowModel();
+    });
+    grid.addEventListener("mouseover", function () {
+      if (mouseDown) {
+        rainbowModel();
+      }
+    });
+    grid.addEventListener("mouseup", function () {
+      mouseDown = false;
+    });
+  });
+}
+//This code will clear the grid
+clear.addEventListener("click", clearGrid);
+function clearGrid() {
+  let grid = Array.from(document.getElementsByClassName("grid-item"));
+  grid.forEach((grid) => {
+    grid.style.backgroundColor = SECONDARYCOLOR;
+  });
+}
+//This code change the slider value
+sizeSlider.addEventListener("input", sizing);
+function sizing() {
+  const value = Number(sizeSlider.value);
+  sizeValue.textContent = `${value} x ${value}`;
+}
+
+//This code below makes the rows and cols
 function makeRows(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
@@ -46,7 +127,6 @@ function makeRows(rows, cols) {
       grid.addEventListener("mouseover", function () {
         if (mouseDown) {
           changeColor();
-          console.log(mouseDown);
         }
       });
       grid.addEventListener("mouseup", function () {
@@ -56,8 +136,4 @@ function makeRows(rows, cols) {
   }
 }
 makeRows(16, 16);
-
-function rainbow() {
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  return randomColor;
-}
+//This code clears the grid
